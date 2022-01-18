68f587882fa038ec9d4de1e3f96693c3df5340c9
kelliekumasaka <kelliek3@gmail.com>
2022-01-17 18:46:37 -0800

    hrefs and project component deletion

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,14 +8,12 @@ function Navbar(){
         <div id="main">
             <nav className="uk-navbar-container" uk-navbar='true' id="navBar">
                 <div className="uk-navbar-left">
-                    <a className="uk-icon-button uk-margin-small-left" id='welcome'><img src={Logo} alt='custom logo'></img></a>
+                    <a className="uk-icon-button uk-margin-small-left" href='#welcome-main' id='welcome'><img src={Logo} alt='custom logo'></img></a>
                 </div>
                 <div className="uk-navbar-right">
                     <ul className="uk-navbar-nav">
-                        <li className="uk-active"><a id='intro'
-                        
-                        >Intro</a></li>
-                        <li className="uk-active"><a id='work' >Work</a></li>
+                        <li className="uk-active"><a id='intro' href="#about">Intro</a></li>
+                        <li className="uk-active"><a id='work' href="#work-container">Work</a></li>
                         <li className="uk-active"><a id='resume' href={Pdf}>Resume</a></li>
                     </ul>
                 </div>
