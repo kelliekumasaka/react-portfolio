5324bea8a0a13309b2ad1bd1dc4e48e4b39e8c28
kelliekumasaka <kelliek3@gmail.com>
2022-01-25 11:41:57 -0800

    about intro complete

--- src/components/pages/Welcome/Welcome.js
+++ src/components/pages/Welcome/Welcome.js
@@ -1,15 +1,19 @@
 import React from "react";
 import Pic from "./new.png"
 import "./Welcome.css"
+import About from "./About";
 
 function Welcome(){
     return(
         <>
             <div id="welcome-main">
                 <div id='welcomeContainer'>
-                    <div id="title-container">
-                        <h1 id="hi" className="uk-animation-fade my-title">Kellie Kumasaka.</h1>
-                        <h1 className="uk-animation-fade my-title">Full-Stack Developer.</h1>
+                    <div id="info-container">
+                        <div id="title-container">
+                            <h1 id="hi" className="uk-animation-fade my-title">Kellie Kumasaka.</h1>
+                            <h1 className="uk-animation-fade my-title">Full-Stack Developer.</h1>
+                        </div>
+                        <About/>
                     </div>
                     <img alt="profile pic" id="myPic" src={Pic} display="inline"/>
                 </div>
