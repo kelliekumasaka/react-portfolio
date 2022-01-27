8c6e00afe856d84e39dabef7fa717849154f6810
kelliekumasaka <kelliek3@gmail.com>
2022-01-27 12:36:36 -0800

    overlays with titles

--- src/pages/Project/Project.js
+++ src/pages/Project/Project.js
@@ -9,46 +9,37 @@ function Project(){
         <div id="projects">
             <div id="project-container">
                 <div id="project-title-container">
-                    <h1 className="project-title"  id="project-header">Projects </h1>
+                    <h1 className="project-title" id="project-header">Projects </h1>
                     <h1 className="project-title">so far...</h1>
                 </div>
                 <div id="grid-container">
-                    {/* <div className="uk-child-width-1-2@m" uk-grid='true'>
-                        <div>
-                            <div className="uk-inline">
-                                <img src={AnimalCrossing} alt=""></img>
-                                <div className="uk-overlay uk-overlay-default uk-position-bottom">
-                                    <p>Default Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="uk-inline">
-                                <img src={BuJo} alt=""></img>
-                                <div className="uk-overlay-default uk-position-bottom"></div>
-                                <div className="uk-overlay uk-position-bottom uk-dark">
-                                    <p>Default Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="uk-inline">
-                                <img src={PetIt} alt=""></img>
-                                <div className="uk-overlay uk-overlay-default uk-position-bottom">
-                                    <p>Default Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div> */}
                     <div className="uk-grid-medium uk-child-width-expand@s uk-text-center" uk-grid='true'>
                         <div>
-                            <div id="pet-it" className="uk-card uk-card-default uk-card-body my-card" style={{backgroundImage: `url(${PetIt})`}}></div>
+                            <div id="pet-it" className="uk-card uk-card-default uk-card-body my-card" style={{backgroundImage: `url(${PetIt})`}}>
+                                <div className="uk-overlay uk-overlay-primary uk-position-cover my-overlay">
+                                </div>
+                                <div class="uk-overlay uk-position-top my-project-text">
+                                    <p>PetIt</p>
+                                </div>
+                            </div>
                         </div>
                         <div>
-                            <div id="bullet-journal-squared" className="uk-card uk-card-default uk-card-body my-card" style={{backgroundImage:`url(${BuJo})`}}></div>
+                            <div id="bullet-journal-squared" className="uk-card uk-card-default uk-card-body my-card" style={{backgroundImage:`url(${BuJo})`}}>
+                                <div className="uk-overlay uk-overlay-primary uk-position-cover my-overlay">
+                                </div>
+                                <div class="uk-overlay uk-position-top my-project-text">
+                                    <p>Bullet Journal Squared</p>
+                                </div>
+                            </div>
                         </div>
                         <div>
-                            <div id="acnh-island-manager" className="uk-card uk-card-default uk-card-body my-card" style={{backgroundImage:`url(${AnimalCrossing})`}}></div>
+                            <div id="acnh-island-manager" className="uk-card uk-card-default uk-card-body my-card" style={{backgroundImage:`url(${AnimalCrossing})`}}>
+                                <div className="uk-overlay uk-overlay-primary uk-position-cover my-overlay">
+                                </div>
+                                <div class="uk-overlay uk-position-top my-project-text">
+                                    <p>Animal Crossing Island Manager</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
