ea7a92504b3f6eb636be7aadc8654186c6753418
kelliekumasaka <kelliek3@gmail.com>
2022-01-28 13:09:06 -0800

    second row of projects

--- src/pages/Project/Project.js
+++ src/pages/Project/Project.js
@@ -3,7 +3,9 @@ import "./Project.css";
 import AnimalCrossing from '../../images/animal-crossing.jpg';
 import PetIt from '../../images/dog-and-person.webp';
 import BuJo from '../../images/journal.jpg';
-import Employee from '../../images/meeting.jpg'
+import Employee from '../../images/meeting.jpg';
+import Library from '../../images/suz.jpg';
+import Viewnify from '../../images/viewnify.jpg';
 
 function Project(){
     return(
@@ -14,7 +16,7 @@ function Project(){
                     <h1 className="project-title">so far...</h1>
                 </div>
                 <div id="grid-container">
-                    <div className="uk-grid-medium uk-child-width-expand@s uk-text-center" uk-grid='true'>
+                    <div className="uk-grid-large uk-child-width-expand@s uk-text-center" uk-grid='true'>
                         <div>
                             <div id="pet-it" className="uk-card uk-card-body my-card uk-visible-toggle " tabIndex={-1} uk-toggle="target: #petit-title; mode: hover" style={{backgroundImage: `url(${PetIt})`}}>
                                 <div className="uk-position-bottom-left my-small-overlay" id="petit-title">
@@ -58,7 +60,7 @@ function Project(){
                             </div>
                         </div>
                     </div>
-                    <div className="uk-grid-medium uk-child-width-expand@s uk-text-center" uk-grid='true'>
+                    <div className="uk-grid-large uk-child-width-expand@s uk-text-center" uk-grid='true'>
                         <div>
                             <div id="employee-tracker" className="uk-card uk-card-body my-card uk-visible-toggle " tabIndex={-1} uk-toggle="target: #employee-title; mode: hover" style={{backgroundImage: `url(${Employee})`}}>
                                 <div className="uk-position-bottom-left my-small-overlay" id="employee-title">
@@ -70,6 +72,35 @@ function Project(){
                                     <p className="my-project-title">Employee Tracker</p>
                                     <p className="my-project-info">This in-terminal app allows the user to create and view employees, departments, and roles within a company database.</p>
                                     <p className="read-more"><a className="read-more" target='_blank' rel='noreferrer' href="https://github.com/kelliekumasaka/employee-tracker">GitHub Repo</a></p>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div>
+                            <div id="library" className="uk-card uk-card-body my-card uk-visible-toggle " tabIndex={-1} uk-toggle="target: #library-title; mode: hover" style={{backgroundImage: `url(${Library})`}}>
+                                <div className="uk-position-bottom-left my-small-overlay" id="library-title">
+                                    <p className='my-project-text'>Book Search Engine</p>
+                                </div>
+                                <div className="uk-overlay uk-overlay-primary uk-position-cover my-overlay uk-invisible-hover">
+                                </div>
+                                <div className="uk-overlay uk-position-top my-project-text uk-invisible-hover">
+                                    <p className="my-project-title">Book Search Engine</p>
+                                    <p className="my-project-info">A website that allows the user to look for books and save them and mark them as read.</p>
+                                    <p className="read-more"><a className="read-more" target='_blank' rel='noreferrer' href="https://github.com/kelliekumasaka/book-search-engine">GitHub Repo</a> | <a href="https://kk-book-search-engine.herokuapp.com/" className="read-more" target='_blank' rel='noreferrer'>Website</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div id="viewnify" className="uk-card uk-card-body my-card uk-visible-toggle " tabIndex={-1} uk-toggle="target: #viewnify-title; mode: hover" style={{backgroundImage: `url(${Viewnify})`}}>
+                                <div className="uk-position-bottom-left my-small-overlay" id="viewnify-title">
+                                    <p className='my-project-text'>Viewnify</p>
+                                </div>
+                                <div className="uk-overlay uk-overlay-primary uk-position-cover my-overlay uk-invisible-hover">
+                                </div>
+                                <div className="uk-overlay uk-position-top my-project-text uk-invisible-hover">
+                                    <p className="my-project-title">Viewnify</p>
+                                    <p className="my-project-info">Viewnify is a movie matching, watch party app that allows you and your friends to swipe, like and dislike movies. You match movies with your friends, and can enjoy a watch party together.</p>
+                                    <p className="read-more"><a className="read-more" target='_blank' rel='noreferrer' href="https://github.com/jwhector/viewnify">GitHub Repo</a> | <a href="http://viewnify.herokuapp.com/" className="read-more" target='_blank' rel='noreferrer'>Website</a></p>
                                 </div>
                             </div>
                         </div>
