831f1728054cf5561a2dad19088a1804d59b4345
kelliekumasaka <kelliek3@gmail.com>
2022-01-27 17:05:56 -0800

    employee tracker project cardf

--- src/pages/Project/Project.js
+++ src/pages/Project/Project.js
@@ -3,6 +3,7 @@ import "./Project.css";
 import AnimalCrossing from '../../images/animal-crossing.jpg';
 import PetIt from '../../images/dog-and-person.webp';
 import BuJo from '../../images/journal.jpg';
+import Employee from '../../images/meeting.jpg'
 
 function Project(){
     return(
@@ -24,7 +25,7 @@ function Project(){
                                 <div className="uk-overlay uk-position-top my-project-text uk-invisible-hover">
                                     <p className="my-project-title">PetIt</p>
                                     <p className="my-project-info">Full stack social networking app allowing users to inform each other on the pet-friendliness of employers and establishments using MySQL, React, Node, and Express.</p>
-                                    <p><a className="read-more" href="https://github.com/dimitermusic/petit-frontend">Read More</a></p>
+                                    <p><a className="read-more"  target='_blank' rel='noreferrer' href="https://github.com/dimitermusic/petit-frontend">Read More</a></p>
                                 </div>
                             </div>
                         </div>
@@ -38,7 +39,7 @@ function Project(){
                                 <div className="uk-overlay uk-position-top my-project-text uk-invisible-hover">
                                     <p className="my-project-title">Bullet Journal Squared</p>
                                     <p className="my-project-info">Bullet Journal Squared is a website that allows users to create an account, write journal entries, and view their past thoughts and feelings.</p>
-                                    <p><a className="read-more" href="https://github.com/Mark-LohseMiranda/bulletjournal">Read More</a></p>
+                                    <p><a className="read-more"  target='_blank' rel='noreferrer' href="https://github.com/Mark-LohseMiranda/bulletjournal">Read More</a></p>
                                 </div>
                             </div>
                         </div>
@@ -52,7 +53,23 @@ function Project(){
                                 <div className="uk-overlay uk-position-top my-project-text uk-invisible-hover">
                                     <p className="my-project-title">Animal Crossing Island Manager</p>
                                     <p className="my-project-info">This website was built to help end-users keep track of their villagers and collectibles for Animal Crossing: New Horizons in one central hub.</p>
-                                    <p><a className="read-more" href="https://github.com/Rallen4/project1-animal-crossing">Read More</a></p>
+                                    <p><a className="read-more"  target='_blank' rel='noreferrer' href="https://github.com/Rallen4/project1-animal-crossing">Read More</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="uk-grid-medium uk-child-width-expand@s uk-text-center" uk-grid='true'>
+                        <div>
+                            <div id="employee-tracker" className="uk-card uk-card-default uk-card-body my-card uk-visible-toggle " tabIndex={-1} uk-toggle="target: #employee-title; mode: hover" style={{backgroundImage: `url(${Employee})`}}>
+                                <div className="uk-position-bottom-left my-small-overlay" id="employee-title">
+                                    <p className='my-project-text'>Employee Tracker</p>
+                                </div>
+                                <div className="uk-overlay uk-overlay-primary uk-position-cover my-overlay uk-invisible-hover">
+                                </div>
+                                <div className="uk-overlay uk-position-top my-project-text uk-invisible-hover">
+                                    <p className="my-project-title">Employee Tracker</p>
+                                    <p className="my-project-info">This in-terminal app allows the user to create and view employees, departments, and roles within a company database.</p>
+                                    <p><a className="read-more" target='_blank' rel='noreferrer' href="https://github.com/kelliekumasaka/employee-tracker">Read More</a></p>
                                 </div>
                             </div>
                         </div>
