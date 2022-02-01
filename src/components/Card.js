207e8641e5644977c7b8497a1b2f03d64dce0abd
kelliekumasaka <kelliek3@gmail.com>
2022-02-01 12:37:03 -0800

    card component props

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './Card.css';
+
+export default function Card(props){
+    return(
+        <div key={props.name} className="uk-width-1-3@l uk-width-1-2@m uk-width-1-1@s">
+            <div className="uk-card uk-card-body my-card  uk-visible-toggle " tabIndex={-1} uk-toggle={`target: #${props.name}-title; mode: hover`} style={{backgroundImage: `url(${props.background})`}}>
+                <div className="uk-position-bottom-left my-small-overlay" id={`${props.name}-title`}>
+                    <p className='my-project-text'>{props.name}</p>
+                </div>
+                <div className="uk-overlay uk-overlay-primary uk-position-cover my-overlay uk-invisible-hover">
+                </div>
+                <div className="uk-overlay uk-position-top my-project-text uk-invisible-hover">
+                    <p className="my-project-title">{props.name}</p>
+                    <p className="my-project-info">{props.description}</p>
+                    <p className="read-more"><a className="read-more"  target='_blank' rel='noreferrer' href={props.repo}>GitHub Repo</a> | <a className="read-more" href={props.website} target='_blank' rel="noreferrer"> Website</a></p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Project/Project.js
+++ src/pages/Project/Project.js
@@ -6,104 +6,71 @@ import BuJo from '../../images/journal.jpg';
 import Employee from '../../images/meeting.jpg';
 import Library from '../../images/suz.jpg';
 import Viewnify from '../../images/viewnify.jpg';
+import Card from '../../components/Card';
 
 function Project(){
+    const myProjects = [
+        {
+            name:'PetIt',
+            image: PetIt,
+            description:"Full stack social networking app allowing users to inform each other on the pet-friendliness of employers and establishments using MySQL, React, Node, and Express.",
+            repo:"https://github.com/dimitermusic/petit-frontend",
+            website:'https://petit-petfriendly.herokuapp.com/'
+        },
+        {
+            name:'Bullet Journal Squared',
+            image: BuJo,
+            description:"Bullet Journal Squared is a website that allows users to create an account, write journal entries, and view their past thoughts and feelings.",
+            repo:"https://github.com/Mark-LohseMiranda/bulletjournal",
+            website:'https://bullet-journal-squared.herokuapp.com/'
+        },
+        {
+            name:'Animal Crossing Island Manager',
+            image: AnimalCrossing,
+            description:"This website was built to help end-users keep track of their villagers and collectibles for Animal Crossing: New Horizons in one central hub.",
+            repo:"https://github.com/Rallen4/project1-animal-crossing",
+            website:'https://rallen4.github.io/project1-animal-crossing/'
+        },
+        {
+            name:'Employee Tracker',
+            image: Employee,
+            description:"This in-terminal app allows the user to create and view employees, departments, and roles within a company database.",
+            repo:"https://github.com/kelliekumasaka/employee-tracker"
+        },
+        {
+            name:'Book Search Engine',
+            image: Library,
+            description:"A website that allows the user to look for books and save them and mark them as read.",
+            repo:"https://github.com/kelliekumasaka/book-search-engine",
+            website:'https://kk-book-search-engine.herokuapp.com/'
+        },
+        {
+            name:'Viewnify',
+            image: Viewnify,
+            description:"Viewnify is a movie matching, watch party app that allows you and your friends to swipe, like and dislike movies. You match movies with your friends, and can enjoy a watch party together.",
+            repo:"https://github.com/jwhector/viewnify",
+            website:'http://viewnify.herokuapp.com/'
+        },
+    ]
+
     return(
         <div id="projects">
             <div id="project-container">
                 <div id="project-title-container">
-                    <h1 className="project-title" id="project-header">Projects </h1>
+                    <h1 id="project-header">Projects </h1>
                     <h1 className="project-title">so far...</h1>
                 </div>
                 <div id="grid-container">
-                    <div className="uk-grid-large uk-child-width-expand@s uk-text-center" uk-grid='true'>
-                        <div>
-                            <div id="pet-it" className="uk-card uk-card-body my-card uk-visible-toggle " tabIndex={-1} uk-toggle="target: #petit-title; mode: hover" style={{backgroundImage: `url(${PetIt})`}}>
-                                <div className="uk-position-bottom-left my-small-overlay" id="petit-title">
-                                    <p className='my-project-text'>PetIt</p>
-                                </div>
-                                <div className="uk-overlay uk-overlay-primary uk-position-cover my-overlay uk-invisible-hover">
-                                </div>
-                                <div className="uk-overlay uk-position-top my-project-text uk-invisible-hover">
-                                    <p className="my-project-title">PetIt</p>
-                                    <p className="my-project-info">Full stack social networking app allowing users to inform each other on the pet-friendliness of employers and establishments using MySQL, React, Node, and Express.</p>
-                                    <p className="read-more"><a className="read-more"  target='_blank' rel='noreferrer' href="https://github.com/dimitermusic/petit-frontend">GitHub Repo</a> | <a className="read-more" href="https://petit-petfriendly.herokuapp.com/" target='_blank' rel="noreferrer"> Website</a></p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div id="bullet-journal-squared" className="uk-card uk-card-body my-card uk-visible-toggle" uk-toggle="target: #bujo-title; mode: hover" tabIndex={-1} style={{backgroundImage:`url(${BuJo})`}}>
-                                <div className="uk-position-bottom-left my-small-overlay" id="bujo-title">
-                                    <p className='my-project-text'>Bullet Journal Squared</p>
-                                </div>
-                                <div className="uk-overlay uk-overlay-primary uk-position-cover my-overlay uk-invisible-hover">
-                                </div>
-                                <div className="uk-overlay uk-position-top my-project-text uk-invisible-hover">
-                                    <p className="my-project-title">Bullet Journal Squared</p>
-                                    <p className="my-project-info">Bullet Journal Squared is a website that allows users to create an account, write journal entries, and view their past thoughts and feelings.</p>
-                                    <p className="read-more" ><a className="read-more"  target='_blank' rel='noreferrer' href="https://github.com/Mark-LohseMiranda/bulletjournal">GitHub Repo</a> | <a className="read-more"  target='_blank' rel='noreferrer' href="https://bullet-journal-squared.herokuapp.com/">Website</a></p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div id="acnh-island-manager" className="uk-card uk-card-body my-card uk-visible-toggle" uk-toggle="target: #animal-crossing-title; mode: hover"  tabIndex={-1} style={{backgroundImage:`url(${AnimalCrossing})`}}>
-                                <div className='uk-position-bottom-left my-small-overlay' id="animal-crossing-title">
-                                    <p className='my-project-text'>Animal Crossing Island Manager</p>
-                                </div>
-                                <div className="uk-overlay uk-overlay-primary uk-position-cover my-overlay uk-invisible-hover">
-                                </div>
-                                <div className="uk-overlay uk-position-top my-project-text uk-invisible-hover">
-                                    <p className="my-project-title">Animal Crossing Island Manager</p>
-                                    <p className="my-project-info">This website was built to help end-users keep track of their villagers and collectibles for Animal Crossing: New Horizons in one central hub.</p>
-                                    <p className="read-more"><a className="read-more"  target='_blank' rel='noreferrer' href="https://github.com/Rallen4/project1-animal-crossing">GitHub Repo</a> | <a className="read-more"  target='_blank' rel='noreferrer' href="https://rallen4.github.io/project1-animal-crossing/">Website</a></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="uk-grid-large uk-child-width-expand@s uk-text-center" uk-grid='true'>
-                        <div>
-                            <div id="employee-tracker" className="uk-card uk-card-body my-card uk-visible-toggle " tabIndex={-1} uk-toggle="target: #employee-title; mode: hover" style={{backgroundImage: `url(${Employee})`}}>
-                                <div className="uk-position-bottom-left my-small-overlay" id="employee-title">
-                                    <p className='my-project-text'>Employee Tracker</p>
-                                </div>
-                                <div className="uk-overlay uk-overlay-primary uk-position-cover my-overlay uk-invisible-hover">
-                                </div>
-                                <div className="uk-overlay uk-position-top my-project-text uk-invisible-hover">
-                                    <p className="my-project-title">Employee Tracker</p>
-                                    <p className="my-project-info">This in-terminal app allows the user to create and view employees, departments, and roles within a company database.</p>
-                                    <p className="read-more"><a className="read-more" target='_blank' rel='noreferrer' href="https://github.com/kelliekumasaka/employee-tracker">GitHub Repo</a></p>
-                                </div>
-                            </div>
-                            
-                        </div>
-                        <div>
-                            <div id="library" className="uk-card uk-card-body my-card uk-visible-toggle " tabIndex={-1} uk-toggle="target: #library-title; mode: hover" style={{backgroundImage: `url(${Library})`}}>
-                                <div className="uk-position-bottom-left my-small-overlay" id="library-title">
-                                    <p className='my-project-text'>Book Search Engine</p>
-                                </div>
-                                <div className="uk-overlay uk-overlay-primary uk-position-cover my-overlay uk-invisible-hover">
-                                </div>
-                                <div className="uk-overlay uk-position-top my-project-text uk-invisible-hover">
-                                    <p className="my-project-title">Book Search Engine</p>
-                                    <p className="my-project-info">A website that allows the user to look for books and save them and mark them as read.</p>
-                                    <p className="read-more"><a className="read-more" target='_blank' rel='noreferrer' href="https://github.com/kelliekumasaka/book-search-engine">GitHub Repo</a> | <a href="https://kk-book-search-engine.herokuapp.com/" className="read-more" target='_blank' rel='noreferrer'>Website</a></p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div id="viewnify" className="uk-card uk-card-body my-card uk-visible-toggle " tabIndex={-1} uk-toggle="target: #viewnify-title; mode: hover" style={{backgroundImage: `url(${Viewnify})`}}>
-                                <div className="uk-position-bottom-left my-small-overlay" id="viewnify-title">
-                                    <p className='my-project-text'>Viewnify</p>
-                                </div>
-                                <div className="uk-overlay uk-overlay-primary uk-position-cover my-overlay uk-invisible-hover">
-                                </div>
-                                <div className="uk-overlay uk-position-top my-project-text uk-invisible-hover">
-                                    <p className="my-project-title">Viewnify</p>
-                                    <p className="my-project-info">Viewnify is a movie matching, watch party app that allows you and your friends to swipe, like and dislike movies. You match movies with your friends, and can enjoy a watch party together.</p>
-                                    <p className="read-more"><a className="read-more" target='_blank' rel='noreferrer' href="https://github.com/jwhector/viewnify">GitHub Repo</a> | <a href="http://viewnify.herokuapp.com/" className="read-more" target='_blank' rel='noreferrer'>Website</a></p>
-                                </div>
-                            </div>
-                        </div>
+                    <div className="uk-grid-large uk-text-center" uk-grid='true'>
+                        {myProjects.map(project => {
+                            return <Card
+                                background={project.image}
+                                name = {project.name}
+                                description = {project.description}
+                                repo = {project.repo}
+                                website = {project.website}
+                            />
+                        })}
                     </div>
                 </div>
             </div>
