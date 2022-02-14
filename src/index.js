4971b46958475175e545578f7bac2a6bd744f7f5
kelliekumasaka <kelliek3@gmail.com>
2022-02-14 14:25:34 -0800

    small changes for live site portfolio

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './reset.css'
 import './index.css';
 import App from './App';
 import '../node_modules/uikit/dist/css/uikit.min.css';
